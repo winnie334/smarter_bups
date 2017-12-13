9681be79b56fcf97e72ed31209e58188a805b5e7
winand appels <winand.appels@hotmail.be>
2017-12-13 13:55:48 +0100

    added some ugly terrain generation

--- sketch.js
+++ sketch.js
@@ -1,19 +1,34 @@
 function setup() {
   createCanvas(1000, 800);
   frameRate(10);
-  field = initfield(50, 50);
-  blocksize = 25;
+  size_x = 50; size_y = 50
+  field = initfield(size_x, size_y);
+  blocksize = Math.min(height / size_y, width / size_x);
 }
 
 function initfield(size_x, size_y) {
+  console.log(size_y / 2);
+  console.log(Math.round(size_y / 2))
   var field = [];
   for (x = 0; x < size_x; x++) {
     var column = [];
     for (y = 0; y < size_y; y++) {
-      if (y != 0 && column[y - 1] == 1 || Math.random() > 0.99
-          || (x != 0 && field[x - 1][y] == 1 && Math.random() > 0.5)) {
+      if (y != 0 && column[y - 1] == 1) {
+      // first we check if there are any ground blocks above it
+        column.push(1);
+      } else if (x == 0 && y == Math.round(size_y / 2)) {
+        // if that's not the case, we need to check if we're in the first column
+        // and put a block at the middle height
+        column.push(1);
+      } else if (x != 0 && y < size_y - 2 && field[x - 1][y + 2] == 1
+                 && Math.random() > 0.7) {
+        // now we check if there's land at about the same height nearby
+        column.push(1);
+      } else if (y > size_y - 4 && Math.random() > 0.1) {
+        // if it's at the bottom of the screen, roll for a random piece of land
         column.push(1);
       } else {
+        // if it fails every check, it's air
         column.push(0);
       }
     }
@@ -27,12 +42,10 @@ function drawfield(field) {
     for (block = 0; block < field[row].length; block++) {
       if (field[row][block] == 0) {
         fill(65, 200, 240);
-        stroke(65, 200, 240);
       }
 
       else if (field[row][block] == 1) {
-        fill(200, 200, 100);
-        stroke(200, 200, 100);
+        fill(70, 40, 20);
       }
       rect(row * blocksize, block * blocksize, blocksize, blocksize);
     }
@@ -41,6 +54,6 @@ function drawfield(field) {
 
 
 function draw() {
-  fill(50, 25, 230);
+  strokeWeight(0);
   drawfield(field);
 }
